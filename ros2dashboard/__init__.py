5fd07eee6ed96158c34fa114e7305dcfae4cfe45
Aleksey Kuchkov <biblbroxxx@gmail.com>
2023-04-25 22:43:18 +0300

    Remove application related files from pages branch

--- ros2dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import configparser
-
-from PySide2.QtGui import QGuiApplication
-from PySide2.QtWidgets import QApplication
-from PySide2 import QtWidgets
-from NodeGraphQt import NodeGraph, BaseNode
-
-import ros2dashboard.app.Ros2Dashboard as Ros2Dashboard
